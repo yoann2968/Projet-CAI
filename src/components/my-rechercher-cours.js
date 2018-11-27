bbb7f6f2cc4d38ad8a53eac9e2ea343fee60a059
l5chaill <chaillan.lukas@gmail.com>
2018-11-27 14:51:05 +0100

    cleaning rechercher cours view

--- src/components/my-rechercher-cours.js
+++ src/components/my-rechercher-cours.js
@@ -1,41 +1,13 @@
-/**
-@license
-Copyright (c) 2018 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
-*/
-
 import { html } from '@polymer/lit-element';
 import { PageViewElement } from './page-view-element.js';
-import { connect } from 'pwa-helpers/connect-mixin.js';
-
-// This element is connected to the Redux store.
-import { store } from '../store.js';
-
-// These are the actions needed by this element.
-import { increment, decrement } from '../actions/counter.js';
-
-// We are lazy loading its reducer.
-import counter from '../reducers/counter.js';
-store.addReducers({
-  counter
-});
-
-// These are the elements needed by this element.
-import './counter-element.js';
-
-// These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
 
-class MyRechercherCours extends connect(store)(PageViewElement) {
+class MyRechercherCours extends (PageViewElement) {
   render() {
     return html`
       ${SharedStyles}
       <section>
-        <p>Compléter cette page avec le formulaire de recherche de cours</p>
+        <p>Rechercher un cours</p>
         <br><br>
       </section>
     `;
