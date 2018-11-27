5bd397e443cacd9bca9d2fc701d8d4177dc12576
l5chaill <chaillan.lukas@gmail.com>
2018-11-27 17:40:32 +0100

    affichage de la liste des professeurs

--- src/components/my-rechercher-cours.js
+++ src/components/my-rechercher-cours.js
@@ -3,14 +3,36 @@ import { PageViewElement } from './page-view-element.js';
 import { SharedStyles } from './shared-styles.js';
 
 class MyRechercherCours extends (PageViewElement) {
+
   render() {
     return html`
-      ${SharedStyles}
-      <section>
-        <p>Rechercher un cours</p>
-        <br><br>
-      </section>
+      <div>
+        ${SharedStyles}
+        ${this.prof.map((item) => html` <section><p>Professeur : ${item.prenom} ${item.nom} </p><br><br></section>`)}
+      </div>
     `;
+  }
+
+  constructor(){
+    super();
+
+    this.prof = new Array();  
+  }
+
+  connectedCallback(){
+    super.connectedCallback();
+    
+    this._getData();
+  }
+
+  async _getData() {
+    try {
+      const response = await fetch('../data/Professeur/jean_du_jardin.json');
+      this.prof = await response.json();
+    }
+    catch (err) {
+      console.log('fetch failed', err);
+    }
   }
 }
 
